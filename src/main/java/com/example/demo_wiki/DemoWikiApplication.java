4137119581eb21608a6fadfe86097ea74369bd2d
ice <3153568637@qqq.com>
2023-03-01 16:26:26 +0800

    2-1 init

---
+++ src/main/java/com/example/demo_wiki/DemoWikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWikiApplication.class, args);
+    }
+
+}
